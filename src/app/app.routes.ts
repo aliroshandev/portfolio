2aac843643c6afb49839244856075a06ed780c10
aliroshan <aliroshan150@gmail.com>
2025-08-10 15:41:02 +0330

    add legal-notice

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,12 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
+    path: 'legal-notice',
+    title: 'Ali Roshanzamir Golafzani | Legal Notice',
+    loadComponent: () => import('@pages/legal-notice/legal-notice.component')
+      .then(m => m.LegalNoticeComponent)
+  },
+  {
     path: '',
     title: 'Ali Roshanzamir Golafzani | Portfolio',
     loadComponent: () => import('@pages/home/home.component')
---
+++ src/app/pages/legal-notice/legal-notice.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {RouterLink} from '@angular/router';
+
+@Component({
+  selector: 'app-legal-notice',
+  imports: [
+    RouterLink
+  ],
+  templateUrl: './legal-notice.component.html',
+  styleUrl: './legal-notice.component.scss'
+})
+export class LegalNoticeComponent {
+
+}
